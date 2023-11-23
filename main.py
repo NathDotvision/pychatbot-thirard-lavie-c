d0ecc91f052be36592ba2113214a5d342360ec36
wek234 <136230630+wek234@users.noreply.github.com>
2023-11-23 18:17:08 +0100

    comment on main.py & fct_names.py

--- main.py
+++ main.py
@@ -7,22 +7,21 @@ Auteur : Téo Lavie & Wagner Thirard
 Date : novembre 2023"""
 
 
-# Importations de modules externes#
-
-
+# Importations des fonctions provenant des autres branches#
 from fct_clean import nettoyer_et_enregistrer_fichiers_dossier_entier
 from fct_names import afficher_noms_presidents_uniques
 
+# Choix des options
 print("Voici la liste des choix possibles :")
 print("1 : Afficher la liste des présidents dont les discours sont disponibles dans le programme \n2 : supprimer la ponctuation dans les fichiers des discours")
 choice = int(input("Quel est votre choix ? "))
 if choice == 1 :
     if __name__ == "__main__":
-        # Répertoire contenant les fichiers originaux
+        # Répertoire contenant les fichiers
         repertoire_entree = './speeches'
-        # Extension des fichiers à considérer
+        # Extension des fichiers à analyser
         extension = '.txt'
-        # Appeler la fonction pour afficher les noms uniques des présidents
+        # Appeler la fonction pour afficher les noms uniques des présidents avec leurs prénoms
         afficher_noms_presidents_uniques(repertoire_entree, extension)
 
 elif choice == 2 :
