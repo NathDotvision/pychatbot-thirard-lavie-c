5f6fabb961845d13f8907694bf1622bba97e1799
wek234 <136230630+wek234@users.noreply.github.com>
2023-11-23 15:37:18 +0100

    crete menu to choose the functunality

--- main.py
+++ main.py
@@ -12,17 +12,27 @@ Date : novembre 2023"""
 
 from fct_names import afficher_noms_presidents_uniques, nettoyer_et_enregistrer_fichiers_dossier_entier
 
-if __name__ == "__main__":
-    # Répertoire contenant les fichiers originaux
-    repertoire_entree = './speeches'
-    # Répertoire de sortie pour les fichiers nettoyés
-    repertoire_sortie = './cleaned'
-    # Extension des fichiers à considérer
-    extension = '.txt'
+print("Que voulez-vous faire ?")
+print("Voici la liste des choix possibles :")
+print("1 : Afficher la liste des présidents dont les discours sont disponibles dans le programme")
+choice = int(input())
+if choice == 1 :
+    if __name__ == "__main__":
+        # Répertoire contenant les fichiers originaux
+        repertoire_entree = './speeches'
+        # Répertoire de sortie pour les fichiers nettoyés
+        repertoire_sortie = './cleaned'
+        # Extension des fichiers à considérer
+        extension = '.txt'
 
-    # Nettoyer et enregistrer les fichiers du répertoire speeches dans le répertoire cleaned
-    nettoyer_et_enregistrer_fichiers_dossier_entier(repertoire_entree, repertoire_sortie)
+        # Nettoyer et enregistrer les fichiers du répertoire speeches dans le répertoire cleaned
+        nettoyer_et_enregistrer_fichiers_dossier_entier(repertoire_entree, repertoire_sortie)
 
-    # Appeler la fonction pour afficher les noms uniques des présidents
-    afficher_noms_presidents_uniques(repertoire_sortie, extension)
+        # Appeler la fonction pour afficher les noms uniques des présidents
+        afficher_noms_presidents_uniques(repertoire_sortie, extension)
+
+
+
+
+
 
