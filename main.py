f6bf5e5b1da4da8f5f25404bad48a6bdb3a13f8f
wek234 <136230630+wek234@users.noreply.github.com>
2023-11-23 19:17:36 +0100

    create a function menu to create a loop in case of a non-defined choice

--- main.py
+++ main.py
@@ -12,27 +12,30 @@ from fct_clean import nettoyer_et_enregistrer_fichiers_dossier_entier
 from fct_names import afficher_noms_presidents_uniques
 
 # Choix des options
-print("Voici la liste des choix possibles :")
-print("1 : Afficher la liste des présidents dont les discours sont disponibles dans le programme \n2 : supprimer la ponctuation dans les fichiers des discours")
-choice = int(input("Quel est votre choix ? "))
-if choice == 1 :
-    if __name__ == "__main__":
-        # Répertoire contenant les fichiers
-        repertoire_entree = './speeches'
-        # Extension des fichiers à analyser
-        extension = '.txt'
-        # Appeler la fonction pour afficher les noms uniques des présidents avec leurs prénoms
-        afficher_noms_presidents_uniques(repertoire_entree, extension)
+def menu():
+    print("Voici la liste des choix possibles :")
+    print("1 : Afficher la liste des présidents dont les discours sont disponibles dans le programme \n2 : supprimer la ponctuation dans les fichiers des discours")
+    choice = int(input("Quel est votre choix ? "))
+    if choice == 1:
+        if __name__ == "__main__":
+            # Répertoire contenant les fichiers
+            repertoire_entree = './speeches'
+            # Extension des fichiers à analyser
+            extension = '.txt'
+            # Appeler la fonction pour afficher les noms uniques des présidents avec leurs prénoms
+            afficher_noms_presidents_uniques(repertoire_entree, extension)
 
-elif choice == 2 :
-    if __name__ == "__main__":
-        # Répertoire contenant les fichiers originaux
-        repertoire_entree = './speeches'
-        # Répertoire de sortie pour les fichiers nettoyés
-        repertoire_sortie = './cleaned'
+    elif choice == 2:
+        if __name__ == "__main__":
+            # Répertoire contenant les fichiers originaux
+            repertoire_entree = './speeches'
+            # Répertoire de sortie pour les fichiers nettoyés
+            repertoire_sortie = './cleaned'
 
-        # Nettoyer et enregistrer les fichiers du répertoire speeches dans le répertoire cleaned
-        nettoyer_et_enregistrer_fichiers_dossier_entier(repertoire_entree, repertoire_sortie)
+            # Nettoyer et enregistrer les fichiers du répertoire speeches dans le répertoire cleaned
+            nettoyer_et_enregistrer_fichiers_dossier_entier(repertoire_entree, repertoire_sortie)
 
-else :
-    print("This choice doesn't exist :/")
+    else:
+        print("This choice doesn't exist :/ \n")
+        return menu()
+menu()
