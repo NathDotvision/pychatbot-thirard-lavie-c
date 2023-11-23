c9c480c85b27e75be16f1e65387048132970581e
wek234 <136230630+wek234@users.noreply.github.com>
2023-11-23 22:05:13 +0100

    modify .gitignore to ignore the .idea folder

--- fct_clean.py
+++ fct_clean.py
@@ -1,6 +1,11 @@
 import os
 import string
-
+def list_of_files(directory, extension):
+    files_names = []
+    for filename in os.listdir(directory):
+        if filename.endswith(extension):
+            files_names.append(filename)
+    return files_names
 def nettoyer_texte(texte):
     # Convertir le texte en minuscules
     texte = texte.lower()
