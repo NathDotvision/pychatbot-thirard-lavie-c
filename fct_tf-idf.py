ccf013900dd49b85c0f6918bba42b8fd6fe5256c
TeoLavie <151037733+TeoLavie@users.noreply.github.com>
2023-11-23 16:46:21 +0100

    modify fct_tf-idf.py

--- fct_tf-idf.py
+++ fct_tf-idf.py
@@ -10,19 +10,20 @@ def list_of_files(directory, extension):
 
 
 
-def calculer_occurrences_mot_dans_fichier(chemin_fichier):
-    for i in range
+def calculer_occurrences_mot(chemin_fichier):
+    list_f=list_of_files(chemin_fichier,'.txt')
     occurrences = {}
+    for fichier in list_f:
 
-    with open(chemin_fichier, 'r', encoding='utf-8') as file:
-        contenu = file.read()
-        for mot in mots:
-                if mot in occurrences:
-                    occurrences[mot] += 1
-                else:
-                    occurrences[mot] = 1
+        with open(chemin_fichier+fichier, 'r', encoding='utf-8') as file:
+            contenu = file.read()
+            for mot in contenu:
+                    if mot in occurrences:
+                        occurrences[mot] += 1
+                    else:
+                        occurrences[mot] = 1
 
-        return occurrences
+    return occurrences
 chemin_du_fichier = './cleaned/'
 resultat_occurrences = calculer_occurrences_mot(chemin_du_fichier)
 
