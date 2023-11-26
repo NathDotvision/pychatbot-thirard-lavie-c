e01c8dabc3d42e2dea8a5b1ceb2a3330537cc8a5
wek234 <136230630+wek234@users.noreply.github.com>
2023-11-26 21:29:30 +0100

    comments and update menu

--- fct_analyze.py
+++ fct_analyze.py
@@ -1,40 +1,42 @@
-import os
-from fct_tf_idf import calculer_tf
-from fct_tf_idf import calculer_tf_idf_matrice
+from fct_tf_idf import *
+# fonction liste des fichiers
+def list_of_files(directory, extension):
+    files_names = []
+    for filename in os.listdir(directory):
+        if filename.endswith(extension):
+            files_names.append(filename)
+    return files_names
 
+# fonction qui calcule la matrice TF-IDF
+directory = './cleaned/'
+tfidf_matrice, tout_mots, list_f = calculer_tf_idf_matrice(directory)
 
 ############################
 #Question 1:
 ############################
 
 
-def mots_non_importants(tfidf_matrice, tout_mots, list_f):
+def mots_non_importants():
+
     # initialise une liste pour stocker les mots non importants
     mots_non_importants = []
-    # boucle sur chaque mot
     for i in range(len(tout_mots)):
         # initialise un booleen pour indiquer si le mot est "non important"
         est_non_important = True
         # vérifie si le score TF-IDF est 0 dans tous les fichiers
         for j in range(len(list_f)):
             if tfidf_matrice[i][j] != 0:
-                    # si le score TF-IDF n'est pas 0 le mot est important
                 est_non_important = False
             # si le mot est non important l'ajoute à la liste
         if est_non_important:
             mots_non_importants.append(tout_mots[i])
-
     return mots_non_importants
 
-directory = './cleaned/'
-# utilise la fonction calcule la matrice TF-IDF
-tfidf_matrice, tout_mots, list_f = calculer_tf_idf_matrice(directory)
-
-#  affiche les mots non importants
-mots_non_importants_liste = mots_non_importants(tfidf_matrice, tout_mots, list_f)
-print("Mots non importants :")
-print(mots_non_importants_liste)
-
+# affiche les mots non importants
+def listniw():
+    mots_non_importants_liste = mots_non_importants()
+    print("Mots non importants :")
+    print(mots_non_importants_liste)
 
 
 ############################
@@ -42,7 +44,9 @@ print(mots_non_importants_liste)
 ############################
 
 
-def plus_grand_idf(tfidf_matrice, tout_mots):
+def plus_grand_tfidf():
+    # appel de la fonction pour trouver le mot ayant le score TF-IDF le plus élevé
+
     max_tfidf = -1
     max_tfidf_words = []
     for i in range(len(tout_mots)):
@@ -56,22 +60,18 @@ def plus_grand_idf(tfidf_matrice, tout_mots):
             max_tfidf_words.append(mot)
 
     return max_tfidf_words, max_tfidf
-# appel de la fonction pour trouver le mot ayant le score TF-IDF le plus élevé
-max_tfidf_words, max_tfidf_value = plus_grand_idf(tfidf_matrice, tout_mots)
-# afficher le mot ayant le score TF-IDF le plus élevé
-print("Mot ayant le score TF-IDF le plus élevé ",max_tfidf_value,":")
-print(max_tfidf_words)
 
+# affiche le mot ayant le score TF-IDF le plus élevé
+def tdfmax():
+    max_tfidf_words, max_tfidf_value = plus_grand_tfidf()
+    print("Mot ayant le score TF-IDF le plus élevé ", max_tfidf_value, ":")
+    print(max_tfidf_words)
 
-def list_of_files(directory, extension):
-    files_names = []
-    for filename in os.listdir(directory):
-        if filename.endswith(extension):
-            files_names.append(filename)
-    return files_names
-
+############################
 # Function n°3
-def mots_plus_repetes_par_chirac(directory):
+############################
+
+def mots_plus_repetes_chirac(directory):
     chirac_occurrences = {}  # Dictionnaire pour stocker les occurrences de chaque mot pour Chirac
     chirac_files = [fichier for fichier in list_of_files(directory, '.txt') if 'Chirac' in fichier]
 
@@ -87,13 +87,17 @@ def mots_plus_repetes_par_chirac(directory):
     mots_plus_repetes = sorted(chirac_occurrences.items(), key=lambda x: x[1], reverse=True)
     return mots_plus_repetes[:10]  # Les 10 mots les plus répétés
 
-directory = './cleaned/'
-mots_repetes_chirac = mots_plus_repetes_par_chirac(directory)
-print("Les mots les plus répétés par le président Chirac :", mots_repetes_chirac)
+# affiche les mots les plus répétés par le président Chirac
+def repete():
+    directory = './cleaned/'
+    mots_repetes_chirac = mots_plus_repetes_chirac(directory)
+    print("Les mots les plus répétés par le président Chirac :", mots_repetes_chirac)
 
+############################
 # Function n°4
+############################
 
-def analyse_occurrences_nation(directory):
+def occurrences_nation(directory):
     presidents_occurrences = {}  # Dictionnaire pour stocker les occurrences de chaque président pour "Nation"
     all_presidents = set()  # Ensemble pour stocker tous les noms de président
 
@@ -114,45 +118,49 @@ def analyse_occurrences_nation(directory):
     if not presidents_occurrences:  # Vérifier si la liste est vide
         return [], None  # Si la liste est vide, retourner des résultats vides
 
-    # Trouver le(s) président(s) qui a (ont) parlé de la "Nation"
+    # Trouve les présidents qui ont parlé de "nation"
     presidents_nation = [president for president, occ in presidents_occurrences.items() if occ > 0]
-    # Trouver le président qui a répété le plus de fois le mot "Nation"
+    # Trouve le président qui a répété le plus de fois le mot "nation"
     president_plus_repete = max(presidents_occurrences, key=presidents_occurrences.get)
 
     return presidents_nation, president_plus_repete
 
-directory = './cleaned/'
-presidents_nation, president_plus_repete = analyse_occurrences_nation(directory)
+# affiche les président qui ont utilisé « nation » dans leurs discours
+def occnation():
+    directory = './cleaned/'
+    presidents_nation, president_plus_repete = occurrences_nation(directory)
 
-if president_plus_repete == "Chirac2" :
-    president_plus_repete = "Chirac"
-if president_plus_repete == "Mitterrand2" :
-    president_plus_repete = "Mitterrand"
-if presidents_nation:
-    print("Président(s) qui ont parlé de la « Nation » :", presidents_nation)
-    print("Président qui l’a répété le plus de fois :", president_plus_repete)
-else:
-    print("Aucun président n'a parlé de la « Nation » dans les discours analysés.")
+    if president_plus_repete == "Chirac2" :
+        president_plus_repete = "Chirac"
+    if president_plus_repete == "Mitterrand2" :
+        president_plus_repete = "Mitterrand"
+    if presidents_nation:
+        print("Président(s) qui ont parlé de la « Nation » :", presidents_nation)
+        print("Président qui l’a répété le plus de fois :", president_plus_repete)
+    else:
+        print("Aucun président n'a parlé de la « Nation » dans les discours analysés.")
 
+############################
 # Function n°5
+############################
 
-def premier_president_climat_ecologie(directory):
+def climat_ecologie(directory):
     all_presidents = set()  # Ensemble pour stocker tous les noms de président
 
     for fichier in list_of_files(directory, '.txt'):
-        # Extraire le nom du président à partir du nom du fichier (supposons que le nom soit avant la première "_")
+        # Extraire le nom du président à partir du nom du fichier
         president = fichier.split('_')[1].split('.')[0]
         all_presidents.add(president)
 
         occurrences = calculer_tf(fichier)
         if "climat" in occurrences or "écologie" in occurrences:
-            return president  # Le premier président à parler du climat et/ou de l'écologie
+            return president  # Le premier président à parler du climat ou de l'écologie
 
-    return None  # Aucun président n'a parlé du climat ou de l'écologie
-
-directory = './cleaned/'
-premier_president = premier_president_climat_ecologie(directory)
-print("Premier président à parler du climat et/ou de l’écologie :", premier_president)
+# affiche le premier président à parler du climat ou de l’écologie
+def climeco():
+    directory = './cleaned/'
+    premier_president = climat_ecologie(directory)
+    print("Premier président à parler du climat et/ou de l’écologie :", premier_president)
 
 
 ############################
@@ -160,9 +168,9 @@ print("Premier président à parler du climat et/ou de l’écologie :", premier
 ############################
 
 
-def mots_communs_tous_presidents(tfidf_matrice, tout_mots, list_f):
+def mots_communs(tfidf_matrice, tout_mots, list_f):
     # initialise une liste pour stocker les mots communs à tous les présidents
-    mots_communs = []
+    mots_comm = []
 
     # boucle sur chaque mot
     for i, mot in enumerate(tout_mots):
@@ -177,14 +185,16 @@ def mots_communs_tous_presidents(tfidf_matrice, tout_mots, list_f):
             j += 1
         # si le mot est commun à tous les présidents l'ajoute à la liste
         if est_commun:
-            mots_communs.append(mot)
-    return mots_communs
-
-directory = './cleaned/'
-# utilise la fonction qui calcule la matrice TF-IDF
-tfidf_matrice, tout_mots, list_f = calculer_tf_idf_matrice(directory)
+            mots_comm.append(mot)
+    return mots_comm
 
 # affiche les mots communs à tous les présidents
-mots_communs_tous_presidents_liste = mots_communs_tous_presidents(tfidf_matrice, tout_mots, list_f)
-print("Mots important communs à tous les présidents :")
-print(mots_communs_tous_presidents_liste)
+def communs():
+    directory = './cleaned/'
+    # utilise la fonction qui calcule la matrice TF-IDF
+    tfidf_matrice, tout_mots, list_f = calculer_tf_idf_matrice(directory)
+
+    # affiche les mots communs à tous les présidents
+    mots_communs_liste = mots_communs(tfidf_matrice, tout_mots, list_f)
+    print("Mots important communs à tous les présidents :")
+    print(mots_communs_liste)
--- main.py
+++ main.py
@@ -4,21 +4,25 @@ PyChat Bot
 Programme projet python semestre 01 L1 Efrei
 
 Auteur : Téo Lavie & Wagner Thirard
-Date : novembre 2023"""
+Date : novembre 2023
+"""
 
 
-# Importations des fonctions provenant des autres branches#
+# Importations des fonctions provenant des autres branches #
 from fct_clean import nettoyer_et_enregistrer_fichiers_dossier_entier
 from fct_names import afficher_noms_presidents_uniques
+from fct_analyze import *
+
+
 
 # Choix des options
 def menu():
     print("Voici la liste des choix possibles :")
-    print("1 : Afficher la liste des présidents dont les discours sont disponibles dans le programme \n2 : supprimer la ponctuation dans les fichiers des discours")
+    print("1 : Afficher la liste des présidents dont les discours sont disponibles dans le programme \n2 : Supprimer la ponctuation dans les fichiers des discours\n3 : Accéder au menu d'analyse")
     choice = int(input("Quel est votre choix ? "))
     if choice == 1:
         if __name__ == "__main__":
-            # Répertoire contenant les fichiers
+            # Répertoire contenant les discours
             repertoire_entree = './speeches'
             # Extension des fichiers à analyser
             extension = '.txt'
@@ -27,15 +31,41 @@ def menu():
 
     elif choice == 2:
         if __name__ == "__main__":
-            # Répertoire contenant les fichiers originaux
+            # Répertoire contenant les discours
             repertoire_entree = './speeches'
             # Répertoire de sortie pour les fichiers nettoyés
             repertoire_sortie = './cleaned'
-
-            # Nettoyer et enregistrer les fichiers du répertoire speeches dans le répertoire cleaned
+            # Nettoyer et enregistre les fichiers de speeches dans cleaned
             nettoyer_et_enregistrer_fichiers_dossier_entier(repertoire_entree, repertoire_sortie)
+
+
+    elif choice == 3 :
+        print("Bienvenue dans le sous-menu d'analyse : \n1 - Afficher la liste des mots les moins importants\n2 - Afficher les mots ayant le score TD-IDF le plus élevé\n3 - Afficher les mots les plus répétés par le président Chirac\n4 - les président qui ont parlé de nation\n5 - le premier président a avoir parlé d'écologie ou de climat\n6 - Afficher les mots que tout les présidents ont évoqués")
+        chan = int(input("Quel est votre choix ? "))
+        if chan == 1 :
+            if __name__ == "__main__":
+                listniw()
+        elif chan == 2 :
+            if __name__ == "__main__":
+                tdfmax()
+        elif chan == 3 :
+            if __name__ == "__main__":
+                repete()
+        elif chan == 4 :
+            if __name__ == "__main__":
+                occnation()
+        elif chan == 5 :
+            if __name__ == "__main__":
+                climeco()
+        elif chan == 6 :
+            if __name__ == "__main__":
+                communs()
+        else :
+            print("This choice doesn't exist :/ \n")
+            return menu()
 
     else:
         print("This choice doesn't exist :/ \n")
         return menu()
+
 menu()
